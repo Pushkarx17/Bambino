ca677324e900be28d149ed4960a295adf54ee064
Pushkarx17 <pushkargowda39923993@gmail.com>
2024-09-18 01:06:01 +0530

    big update

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function returnreply(){
     let input = document.getElementById("userreply").value;
     alert(input)
 }
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
